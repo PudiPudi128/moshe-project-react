5d4f53cc1116e3d115fa5f91c4e7f19d204e7f49
Moshe Rokach <mosherokach111@gmail.com>
2023-03-29 19:14:19 +0300

    ok

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const Layout = () => {
-  return <div>Layout</div>;
-};
+  return (
+    <div>
+        <Navbar/>
+        <Outlet/>
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cars from "../components/Cars";
+import Home from "../components/Home";
+import Layout from "../layout/Layout";
+import NotFound404 from "../layout/NotFound404";
 
 const AppRoutes = () => {
-  return <div>AppRoutes</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cars" element={<Cars />} />
+        </Route>
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRoutes;
