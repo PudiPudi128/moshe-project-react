81850331d5457518e5292dc8f19f15ca4fbdbbf4
Moshe Rokach <mosherokach111@gmail.com>
2023-03-29 19:17:22 +0300

    ok

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cars from "../components/Cars";
 import Home from "../components/Home";
 import Layout from "../layout/Layout";
 import NotFound404 from "../layout/NotFound404";
@@ -11,7 +10,6 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/cars" element={<Cars />} />
         </Route>
         <Route path="*" element={<NotFound404 />} />
       </Routes>
