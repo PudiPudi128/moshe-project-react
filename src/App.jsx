ffd436fcfd9b493e81cfe899c70ad1fff1721446
Moshe Rokach <mosherokach111@gmail.com>
2023-03-29 19:15:34 +0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
 }
+
+export default App;
